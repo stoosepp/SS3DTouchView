8d3ada66cb0331923e081a4085d242411a62235e
Stuart Sepp <stoosepp@gmail.com>
2016-01-08 16:02:01 -0800

    Initial Commit

---
+++ SS3DTouchViewUITests/SS3DTouchViewUITests.swift
@@ -0,0 +1,36 @@
+//
+//  SS3DTouchViewUITests.swift
+//  SS3DTouchViewUITests
+//
+//  Created by Stoo on 2016-01-08.
+//  Copyright © 2016 Monotreme. All rights reserved.
+//
+
+import XCTest
+
+class SS3DTouchViewUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
